33777c9c20c5e74116213172d5b0969ccef8bb09
stmult <stmult@gmail.com>
2022-10-16 18:12:03 +0300

    4.2. Заголовок в файле java [#159036 #352194]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Stas Vid");
+        System.out.println("21.06.1990");
+    }
+}
