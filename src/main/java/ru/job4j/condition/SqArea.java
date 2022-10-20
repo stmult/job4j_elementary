5399a7b48c9549a885cceb576e79604c1f855f5d
stmult <stmult@gmail.com>
2022-10-20 12:08:21 +0300

    4. Тесты для стороны прямоугольника. [#159460]

--- src/main/java/ru/job4j/condition/SqArea.java
+++ src/main/java/ru/job4j/condition/SqArea.java
@@ -1,10 +1,9 @@
 package ru.job4j.condition;
 
 public class SqArea {
-    public static double square(double p, double k) {
+    public static double square(int p, double k) {
         double h = p / (2 * (k + 1));
-        double l = h * k;
-        double rsl = l * h;
+        double rsl = (h * k) * h;
         return rsl;
     }
 
