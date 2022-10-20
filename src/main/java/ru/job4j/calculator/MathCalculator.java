1d5aeaad4355ce8897c4a671bbc491f5e0e0fc32
stmult <stmult@gmail.com>
2022-10-20 10:49:32 +0300

    1.1. Что такое import [#504773]

---
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -0,0 +1,30 @@
+package ru.job4j.calculator;
+
+import static ru.job4j.math.MathFunction.*;
+
+@SuppressWarnings("checkstyle:EmptyLineSeparator")
+public class MathCalculator {
+
+    public static double sumAndMultiply(double first, double second) {
+        return sum(first, second)
+                + multiply(first, second);
+    }
+
+    public static double differenceAndDivision(double first, double second) {
+        return difference(first, second)
+                + division(first, second);
+    }
+
+    public static double sumAndMultiplyDifferenceAndDivision(double first, double second) {
+        return sum(first, second)
+                + multiply(first, second)
+                + difference(first, second)
+                + division(first, second);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
+        System.out.println("Результат расчета равен: " + differenceAndDivision(10, 20));
+        System.out.println("Результат расчета равен: " + sumAndMultiplyDifferenceAndDivision(10, 20));
+    }
+}
