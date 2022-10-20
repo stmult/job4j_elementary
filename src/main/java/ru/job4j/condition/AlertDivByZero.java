74a9a66f6582f1d12a9d4367ce7c1d8726921b0c
stmult <stmult@gmail.com>
2022-10-20 12:42:40 +0300

    4.1. Операторы ветвлений [#3611]

--- src/main/java/ru/job4j/condition/AlertDivByZero.java
+++ src/main/java/ru/job4j/condition/AlertDivByZero.java
@@ -13,7 +13,7 @@ public class AlertDivByZero {
         }
 
         if (number < 0) {
-            System.out.println("This is negative number");
+            System.out.println("This is negative number.");
         }
     }
 }
