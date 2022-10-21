a1d058f83b5ffd8f5a79ef4646693eae0279e6b1
stmult <stmult@gmail.com>
2022-10-21 09:11:45 +0300

    3. Тесты для определения расстояния между точками. [#159459 #353497]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -38,7 +38,7 @@ public class PointTest {
     }
 
     @Test
-    public void when00to31then316() {
+    public void when00to31then3dot16() {
         double expected = 3.16;
         int x1 = 0;
         int y1 = 0;
